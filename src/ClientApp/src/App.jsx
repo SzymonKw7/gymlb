097a9c894cee69bf4ed3a25e7a78e07a4390b7cf
Damian Osinski <osinskidamian03@gmail.com>
2024-03-06 15:01:32 +0100

    Add /User to setupProxy

--- src/ClientApp/src/App.jsx
+++ src/ClientApp/src/App.jsx
@@ -1,4 +1,15 @@
 function App() {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'User', true);
+    xhr.send();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                console.log(xhr.responseText);
+            }
+        }
+    }
+
     return <div>
         <h1>Hello, world!</h1>
     </div>
