722cb4222d69f04aac560bdb4b77ca6be38b4d0f
Damian Osinski <osinskidamian03@gmail.com>
2024-03-06 16:03:00 +0100

    Add basic styling

--- src/ClientApp/src/index.js
+++ src/ClientApp/src/index.js
@@ -3,7 +3,7 @@ import reportWebVitals from './reportWebVitals';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from "./App.jsx";
-import './index.css'
+import './css/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
