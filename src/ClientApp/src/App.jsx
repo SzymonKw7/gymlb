841c41217ceb2924219d4a8bff6c875f1425db38
Damian Osinski <osinskidamian03@gmail.com>
2024-03-12 15:34:23 +0100

    Add font and bg-image

--- src/ClientApp/src/App.jsx
+++ src/ClientApp/src/App.jsx
@@ -1,15 +1,6 @@
-function App() {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'User', true);
-    xhr.send();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-                console.log(xhr.responseText);
-            }
-        }
-    }
+import "./css/App.css";
 
+function App() {
     return <div>
         <h1>Hello, world!</h1>
     </div>
